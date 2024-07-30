c92a58606486583e7c724a3b46f72ae05114e326
Himal Adhikari <himalad321@gmail.com>
2024-07-30 15:08:26 +0545

    Ported to CPP

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <ctype.h>
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
+#include <time.h>
 
 #define LENGTH 5
 // This is the size of the hash=table
-#define WORDS_SIZE 4000 
+#define WORDS_SIZE 4000
 #define VALID_WORDS_SIZE 20000
 
 #define GUESSES 6
@@ -17,28 +17,28 @@
 #define WRONG 2
 
 // ANSI color codes for boxed in letters
-#define GREEN   "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m"
-#define YELLOW  "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m"
-#define RED     "\e[38;2;255;255;255;1m\e[48;2;220;20;60;1m"
-#define RESET   "\e[0;39m"
+#define GREEN "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m"
+#define YELLOW "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m"
+#define RED "\e[38;2;255;255;255;1m\e[48;2;220;20;60;1m"
+#define RESET "\e[0;39m"
 
-// Represents a node in a hash table, the pointer in it exists for dealing with collision which is handled using a linked list
+// Represents a node in a hash table, the pointer in it exists for dealing with
+// collision which is handled using a linked list
 typedef struct node {
   char word[LENGTH + 1];
-  struct node* next;
-}
-node;
+  struct node *next;
+} node;
 
-unsigned long hash(char* word, unsigned long size);
-bool load(char* file, node** table, unsigned long size); 
-bool exists(char word[], unsigned long table_size, node** table);
+unsigned long hash(char *word, unsigned long size);
+bool load(char *file, node **table, unsigned long size);
+bool exists(char word[], unsigned long table_size, node **table);
 void print_word(char word[], int letters_correctness[]);
-void load_word(unsigned long index, char guess[], node** table);
+void load_word(unsigned long index, char guess[], node **table);
 void check_word(char word[], char guess[], int letter_correctness[]);
 
 int main() {
-  node* table[WORDS_SIZE];
-  node* valid_words[VALID_WORDS_SIZE];
+  node *table[WORDS_SIZE];
+  node *valid_words[VALID_WORDS_SIZE];
   for (int i = 0; i < VALID_WORDS_SIZE; i++) {
     valid_words[i] = NULL;
   }
@@ -66,8 +66,8 @@ int main() {
 
   int guesses = 0;
 
-  printf(GREEN"Welcome to Wordle!\n"RESET);
-  printf(YELLOW"You have %d guesses to guess the word\n"RESET, GUESSES);
+  printf(GREEN "Welcome to Wordle!\n" RESET);
+  printf(YELLOW "You have %d guesses to guess the word\n" RESET, GUESSES);
 
   int letters_correctness[LENGTH];
   bool won = false;
@@ -82,40 +82,38 @@ int main() {
       i++;
     }
     if (strlen(word) != 5) {
-      printf(RED"Please enter a five letter word\n"RESET);
+      printf(RED "Please enter a five letter word\n" RESET);
       continue;
     }
     if (!exists(word, VALID_WORDS_SIZE, valid_words)) {
-      printf(RED"Word Not Found\n"RESET);
+      printf(RED "Word Not Found\n" RESET);
       continue;
     }
     if (strcmp(word, guess) == 0) {
       won = true;
       break;
-    }
-    else {
+    } else {
       check_word(word, guess, letters_correctness);
       guesses += 1;
     }
     print_word(word, letters_correctness);
   }
   if (won) {
-    printf(GREEN"That is the correct guess\n");
-    printf("CONGRATULATIONS!!! You Win\n"RESET);
-  }
-  else {
-    printf("The right answer was "GREEN"%s\n"RESET, guess);
+    printf(GREEN "That is the correct guess\n");
+    printf("CONGRATULATIONS!!! You Win\n" RESET);
+  } else {
+    printf("The right answer was " GREEN "%s\n" RESET, guess);
   }
   return 0;
 }
 
-bool load(char* file, node** table, unsigned long size) {
-  FILE* fptr;
+bool load(char *file, node **table, unsigned long size) {
+  FILE *fptr;
   fptr = fopen(file, "r");
   if (fptr == NULL) {
     return false;
   }
-   
+
   for (int i = 0; i < size; i++) {
     table[i] = NULL;
   }
@@ -124,7 +122,7 @@ bool load(char* file, node** table, unsigned long size) {
 
   while (fscanf(fptr, "%s", word) != EOF) {
     unsigned long hash_value = hash(word, size);
-    node* new = malloc(sizeof(node));
+    node *new = malloc(sizeof(node));
     strcpy(new->word, word);
     new->next = table[hash_value];
     table[hash_value] = new;
@@ -134,18 +132,18 @@ bool load(char* file, node** table, unsigned long size) {
   return true;
 }
 
-unsigned long hash(char* word, unsigned long size) {
+unsigned long hash(char *word, unsigned long size) {
   unsigned long i = 0, hash_value = 0;
   while (word[i] != '\0') {
     hash_value += word[i] * (i + 100);
     i++;
   }
   return hash_value % size;
-} 
+}
 
-bool exists(char word[], unsigned long table_size, node** table) {
+bool exists(char word[], unsigned long table_size, node **table) {
   unsigned long hash_value = hash(word, table_size);
-  node* cursor = table[hash_value];
+  node *cursor = table[hash_value];
   if (cursor == NULL) {
     return false;
   }
@@ -154,31 +152,28 @@ bool exists(char word[], unsigned long table_size, node** table) {
       return true;
     }
     cursor = cursor->next;
-  }while (cursor != NULL);
+  } while (cursor != NULL);
   return false;
 }
 
 void print_word(char word[], int letters_correctness[]) {
   for (int i = 0; i < LENGTH; i++) {
     if (letters_correctness[i] == CORRECT) {
-      printf(GREEN"%c ", word[i]);
-    }
-    else if (letters_correctness[i] == CLOSE) {
-      printf(YELLOW"%c ", word[i]);
-    }
-    else {
-      printf(RED"%c ", word[i]);
+      printf(GREEN "%c ", word[i]);
+    } else if (letters_correctness[i] == CLOSE) {
+      printf(YELLOW "%c ", word[i]);
+    } else {
+      printf(RED "%c ", word[i]);
     }
   }
-  printf(RESET"\n");
+  printf(RESET "\n");
 }
 
-void load_word(unsigned long index, char guess[], node** table) {
+void load_word(unsigned long index, char guess[], node **table) {
   if (table[index]->next == NULL) {
     strcpy(guess, table[index]->word);
-  }
-  else {
-    node* cursor = table[index]->next;
+  } else {
+    node *cursor = table[index]->next;
     unsigned int words_in_linked_list = 1;
 
     do {
---
+++ main.cpp
@@ -0,0 +1,219 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+// Constants
+constexpr int LENGTH = 5;
+constexpr int WORDS_SIZE = 4000;
+constexpr int VALID_WORDS_SIZE = 20000;
+constexpr int GUESSES = 6;
+constexpr int CORRECT = 0;
+constexpr int CLOSE = 1;
+constexpr int WRONG = 2;
+
+// ANSI color codes for boxed-in letters
+const std::string GREEN = "\e[38;2;255;255;255;1m\e[48;2;106;170;100;1m";
+const std::string YELLOW = "\e[38;2;255;255;255;1m\e[48;2;201;180;88;1m";
+const std::string RED = "\e[38;2;255;255;255;1m\e[48;2;220;20;60;1m";
+const std::string RESET = "\e[0;39m";
+
+// Hash Table Node Class
+class Node {
+public:
+  std::string word;
+  Node *next;
+
+  Node(std::string &w) : word(w), next(nullptr) {}
+};
+
+// Hash Table Class
+class HashTable {
+private:
+  std::vector<Node *> table;
+  unsigned long size;
+
+  unsigned long hash(const std::string &word) const {
+    unsigned long hash_value = 0;
+    for (size_t i = 0; i < word.size(); ++i) {
+      hash_value += word[i] * (i + 100);
+    }
+    return hash_value % size;
+  }
+
+public:
+  HashTable(unsigned long s) : size(s) { table.resize(size, nullptr); }
+
+  ~HashTable() {
+    for (Node *head : table) {
+      Node *cursor = head;
+      while (cursor) {
+        Node *temp = cursor;
+        cursor = cursor->next;
+        delete temp;
+      }
+    }
+  }
+
+  bool load(const std::string &file) {
+    std::ifstream fptr(file);
+    if (!fptr)
+      return false;
+
+    std::string word;
+    while (fptr >> word) {
+      unsigned long hash_value = hash(word);
+      Node *new_node = new Node(word);
+      new_node->next = table[hash_value];
+      table[hash_value] = new_node;
+    }
+    return true;
+  }
+
+  bool exists(const std::string &word) {
+    unsigned long hash_value = hash(word);
+    Node *cursor = table[hash_value];
+    while (cursor) {
+      if (cursor->word == word) {
+        return true;
+      }
+      cursor = cursor->next;
+    }
+    return false;
+  }
+
+  std::string get_word(unsigned long index) {
+    Node *cursor = table[index];
+    if (cursor == nullptr)
+      return "";
+
+    std::vector<std::string> words;
+    while (cursor) {
+      words.push_back(cursor->word);
+      cursor = cursor->next;
+    }
+
+    if (words.empty())
+      return "";
+
+    unsigned int element_index = rand() % words.size();
+    return words[element_index];
+  }
+};
+
+// Functions
+void print_word(std::string &word, std::vector<int> &letters_correctness) {
+  for (int i = 0; i < word.size(); i++) {
+    if (letters_correctness[i] == CORRECT) {
+      std::cout << GREEN << word[i] << ' ' << RESET;
+    } else if (letters_correctness[i] == CLOSE) {
+      std::cout << YELLOW << word[i] << ' ' << RESET;
+    } else {
+      std::cout << RED << word[i] << ' ' << RESET;
+    }
+  }
+  std::cout << '\n';
+}
+
+void check_word(std::string &word, std::string &guess,
+                std::vector<int> &letter_correctness) {
+  std::fill(letter_correctness.begin(), letter_correctness.end(), WRONG);
+
+  // Check for correct letters
+  for (int i = 0; i < LENGTH; i++) {
+    if (word[i] == guess[i]) {
+      letter_correctness[i] = CORRECT;
+    }
+  }
+
+  // Check for close letters
+  for (int i = 0; i < LENGTH; i++) {
+    if (letter_correctness[i] != WRONG)
+      continue;
+
+    bool letter_is_close = false;
+    for (int j = 0; j < LENGTH; j++) {
+      if (word[i] == guess[j] && letter_correctness[j] != CORRECT) {
+        letter_is_close = true;
+        break;
+      }
+    }
+    if (letter_is_close) {
+      letter_correctness[i] = CLOSE;
+    }
+  }
+}
+
+// Main Function
+int main() {
+  std::srand(std::time(nullptr));
+
+  HashTable words_table(WORDS_SIZE);
+  HashTable valid_words_table(VALID_WORDS_SIZE);
+
+  if (!words_table.load("words.txt")) {
+    std::cerr << "Couldn't open file `words.txt`\n";
+    return 1;
+  }
+
+  if (!valid_words_table.load("valid_words.txt")) {
+    std::cerr << "Couldn't open file `valid_words.txt`\n";
+    return 1;
+  }
+
+  unsigned long index = std::rand() % WORDS_SIZE;
+  while (words_table.get_word(index).empty()) {
+    index = std::rand() % WORDS_SIZE;
+  }
+
+  std::string guess = words_table.get_word(index);
+  std::string input_word;
+  int guesses = 0;
+  bool won = false;
+
+  std::cout << GREEN << "Welcome to Wordle!\n" << RESET;
+  std::cout << YELLOW << "You have " << GUESSES
+            << " guesses to guess the word\n"
+            << RESET;
+
+  std::vector<int> letters_correctness(LENGTH);
+
+  while (guesses < GUESSES) {
+    std::cout << "Enter a " << LENGTH << " letter word: ";
+    std::getline(std::cin, input_word);
+    std::transform(input_word.begin(), input_word.end(), input_word.begin(),
+                   ::tolower);
+
+    if (input_word.size() != LENGTH) {
+      std::cout << RED << "Please enter a five letter word\n" << RESET;
+      continue;
+    }
+
+    if (!valid_words_table.exists(input_word)) {
+      std::cout << RED << "Word Not Found\n" << RESET;
+      continue;
+    }
+
+    if (input_word == guess) {
+      won = true;
+      break;
+    } else {
+      check_word(input_word, guess, letters_correctness);
+      ++guesses;
+    }
+    print_word(input_word, letters_correctness);
+  }
+
+  if (won) {
+    std::cout << GREEN << "That is the correct guess\n";
+    std::cout << "CONGRATULATIONS!!! You Win\n" << RESET;
+  } else {
+    std::cout << "The right answer was " << GREEN << guess << RESET << '\n';
+  }
+
+  return 0;
+}
